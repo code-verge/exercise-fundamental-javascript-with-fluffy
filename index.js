ef2b8653dc7e7e094f13fff63ed3f5e03f2b6c43
jani500 <jani@stptrade.com>
2024-08-27 18:31:23 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+// Task 1: Variables
+
+
+
+// Task 2: Primitive Data Types
+
+
+
+
+// Task 3: String Manipulation
+
+
+
+
+// Task 4: Operators and Conditionals
+
+
+
+
+// Task 5: Control Structures
+
+
+
+
+// (Bonus) Task 6: Fluffy’s Rainy Day Indoor Games
+
+
+
+
